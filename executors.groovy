1b6d997bba3d863df0ca6b2b6f59ffd84437f340
TopJ <jessetoporowski@gmail.com>
2025-05-25 03:24:45 -0700

    fixed case on Dockerfile

--- executors.groovy
+++ executors.groovy
@@ -1,2 +1,6 @@
 import jenkins.model.*
 Jenkins.instance.setNumExecutors(0) // Recommended to not run builds on the built-in node
+
+
+// From the official jenkins github documentation
+// https://github.com/jenkinsci/docker
